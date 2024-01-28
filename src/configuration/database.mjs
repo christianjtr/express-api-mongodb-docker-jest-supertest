6caf19d6bc21d290b43c788e26b495563cfe77f4
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-01-28 23:06:04 +0100

    Enhance DDBB config implementation

--- src/configuration/database.mjs
+++ src/configuration/database.mjs
@@ -1,10 +1,35 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose';
 import * as models from '../models';
 
-mongoose.Promise = global.Promise;
+const configuration = {
+  URL: `${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`,
+  models,
+};
+
+const connectDB = () => {
+  const DBConnection = mongoose.connection;
+
+  DBConnection.once('open', () => {
+    console.log(`MongoDB database connected: ${configuration.URL}`);
+  });
+
+  DBConnection.on('error', (error) => {
+    console.error(`MongoDB connection error: ${error}`);
+  });
+
+  try {
+    mongoose.connect(configuration.URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 export default {
-  URL: `${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`,
-  mongoose,
-  models,
+  connectDB,
 };
